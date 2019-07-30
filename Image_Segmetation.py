61757e9d85b2e8220d58b7d57591c3c346721c1b
Shubham_AI <shubham.aiexpert@gmail.com>
2019-07-30 14:41:22 +0530

    Adding Comments✔

--- Image_Segmetation.py
+++ Image_Segmetation.py
@@ -1,13 +1,10 @@
+# Importing Libraries
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 import math
 
-video_capture = cv2.VideoCapture(0)
-
-lower = 115
-upper = 235
-distance = 0.0
+# Importing Cascades
 face_cascade = cv2.CascadeClassifier('Cascade_Files/haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier('Cascade_Files/haarcascade_eye.xml')
 smile_cascade = cv2.CascadeClassifier('Cascade_Files/haarcascade_smile.xml')
@@ -18,6 +15,7 @@ palm_cascade = cv2.CascadeClassifier('Cascade_Files/rpalm_cascade.xml')
 yield_sign_cascade = cv2.CascadeClassifier('Cascade_Files/yield_sign_cascade.xml')
 traffic_light_cascade = cv2.CascadeClassifier('Cascade_Files/traffic_light_cascade.xml')
 
+# Making function to detect 
 def detect(gray, frame):
     signs = sign_cascade.detectMultiScale(gray, 1.3, 5)
     hands = hand_cascade.detectMultiScale(gray, 1.3, 5)
@@ -26,8 +24,6 @@ def detect(gray, frame):
     yield_sign = yield_sign_cascade.detectMultiScale(gray, 1.3, 5)
     faces = face_cascade.detectMultiScale(gray, 1.3, 7)
     traffic_light = traffic_light_cascade.detectMultiScale(gray, 1.3, 7)
-   
-   
 
 
     for (x, y, w, h) in faces:
@@ -67,36 +63,31 @@ def detect(gray, frame):
         cv2.putText(frame, 'Traffic_Light', (x, y), cv2.FONT_ITALIC, 1, (255, 255, 255), 2)
         cv2.rectangle(frame, (x, y), (x + w, y + h), (100, 100, 100), 2)
 
-
     return frame
 
-
+# Stating Camera
+video_capture = cv2.VideoCapture(0) # 0, if you have internal camera, 1 if you have externel camera
 
 while True:
     _, frame = video_capture.read()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Also An Edge Detector in another  frame
+    lower = 115
+    upper = 235
     canvas = cv2.Canny(gray, lower, upper)
 
-    hav = cv2.cvtColor(frame, cv2.COLOR_RGB2HSV)
-    h = hav[:, :, 0]
-    s = hav[:, :, 1]
-    v = hav[:, :, 2]  
-
-    lower_skin = np.array([60, 40, 70])
-    higher_skin = np.array([ 200, 240, 200])
-
-    mask_rgb = cv2.inRange(frame, lower_skin, higher_skin)
     cv2.imshow('Canny Edge Detector', canvas)
     detected = detect(gray, frame)
     cv2.imshow('Video', detected)
 
-
+    # Press q to break the loop
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
 video_capture.release()
 cv2.destroyAllWindows()
-Print('Done')
+print('Done')
 
 
 
